dc6f1e63b56349d7a2800661548bd6d4d285be2b
RdeRausseo <amrausseo20@gmail.com>
2025-07-28 00:56:21 +0000

    anadiendo la lógica del contador simple, dividiendo en cada div según la unidad que toque

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SecondsCounter from "./SecondsCounter";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { useEffect, useState } from "react";
 
 //create your first component
 const Home = () => {
+
+	const [cont, setCont] = useState(0); 
+
+	useEffect( ()=>{
+		const interval = setInterval(() => setCont(cont + 1), 1000)
+	})
+
 	return (
-		<SecondsCounter/>
+		<SecondsCounter />
 	);
 };
 
--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -2,21 +2,22 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClock } from '@fortawesome/free-solid-svg-icons'
 
-const SecondsCounter = (time) => {
+const SecondsCounter = ({ time }) => {
 
     const myStyle = {
         border: "1px solid black"
     }
+
     return (
         <div className='d-flex justify-content-center p-5'>
             <div className='d-flex gap-1 text-center  p-5'>
                 <div> <FontAwesomeIcon icon={faClock} /> </div>
-                <div className="" style={myStyle}> {time} </div>
-                <div style={myStyle}> 0 </div>
-                <div style={myStyle}> 0 </div>
-                <div style={myStyle}> 0 </div>
-                <div style={myStyle}> 0 </div>
-                <div style={myStyle}> 0 </div>
+                <div className="" style={myStyle}> {time} %10 </div>
+                <div className="" style={myStyle}> {Math.floor(time /10) %10 } </div>
+                <div className="" style={myStyle}> {Math.floor(time /100) %10 } </div>
+                <div className="" style={myStyle}> {Math.floor(time /1000) %10 } </div>
+                <div className="" style={myStyle}> {Math.floor(time /10000) %10 } </div>
+                <div className="" style={myStyle}> {Math.floor(time /100000) %10 } </div>
             </div>
         </div>
 
